3b7fdbe2df8f3165b40de3e1da4a6151bcf14743
itsuki-jp <itsuki19601@gmail.com>
2022-01-20 16:44:49 +0900

    コメントの追加

--- omikuji.js
+++ omikuji.js
@@ -9,8 +9,8 @@ if (username == "") {
     document.getElementById("name").innerHTML = username;
 }
 
-
 let rand = Math.floor(Math.random() * 5);
+// if文で書くよりも、大吉、中吉...を配列で管理し、取得したほうが短くて良い
 let result = ["大吉", "中吉", "小吉", "吉", "凶"]
 
 document.getElementById("result").innerHTML = result[rand];
